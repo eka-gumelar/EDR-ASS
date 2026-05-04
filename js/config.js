605a143ac799cc158b333be857bd64ffa5e3f596
eka-gumelar <miracle.khae@gmail.com>
2026-05-04 19:19:17 +0700

    Refactor Firebase configuration in config.js
    Removed Firebase initialization code from config.js.

--- js/config.js
+++ js/config.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDfWp9DMuoDDr7M7LIfgQfok8xKmn4yOe0",
   authDomain: "edr-assembling.firebaseapp.com",
@@ -14,5 +8,3 @@ const firebaseConfig = {
   appId: "1:156144912420:web:f5c04f476a902dae1c70b1"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
