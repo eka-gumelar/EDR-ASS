a0b13710f447b3402488e55f7cdb5d5ca93f914e
eka-gumelar <miracle.khae@gmail.com>
2026-05-05 10:23:04 +0700

    Add files via upload

---
+++ js/data-leader.js
@@ -0,0 +1,11 @@
+    const masterDataLeader = [
+        { lisensi: "AS0001", pass: "1000SA", nama: "Abdul", line: "Line 1", shift: "A", target: 5000, last_edited: "2024-05-01" },
+        { lisensi: "AS0002", pass: "2000SA", nama: "Budi", line: "Line 1", shift: "B", target: 5000, last_edited: "2024-05-01" },
+        { lisensi: "AS0003", pass: "3000SA", nama: "Cahyo", line: "Line 2", shift: "A", target: 5000, last_edited: "2024-05-01" },
+        { lisensi: "AS0004", pass: "4000SA", nama: "Dodi", line: "Line 2", shift: "B", target: 5000, last_edited: "2024-05-01" },
+        { lisensi: "AS0005", pass: "5000SA", nama: "Erni", line: "Line 3", shift: "A", target: 5000, last_edited: "2024-05-01" },
+        { lisensi: "AS0006", pass: "6000SA", nama: "Fitri", line: "Line 3", shift: "B", target: 5000, last_edited: "2024-05-01" },
+        { lisensi: "AS0007", pass: "7000SA", nama: "Gunawan", line: "Line 4", shift: "A", target: 5000, last_edited: "2024-05-01" },
+        { lisensi: "AS0008", pass: "8000SA", nama: "Hadid", line: "Line 4", shift: "B", target: 5000, last_edited: "2024-05-01" },
+        { lisensi: "111", pass: "111", nama: "Demo", line: "Line 1", shift: "NS", target: 10000, last_edited: "2024-05-01" }
+    ];
